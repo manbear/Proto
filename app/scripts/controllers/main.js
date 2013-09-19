df3142f031d338a26ae20cb0cf5b4bb9d8436e00
Guillaume Massé <masgui@gmail.com>
2013-09-19 15:29:14 -0400

    typesafe color pallet

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,18 +2,10 @@
 
 angular.module('protoApp')
   .controller('CodeCtrl', function ($scope) {
-    $scope.themes = ['solarized dark', 'solarized light', 'ambiance'];
-    $scope.theme = $scope.themes[0];
-    $scope.code = 'println(hello world)';
+    $scope.code = 'val a = 1 + 1';
+    $scope.insight = 'a = 2';
     $scope.options = {
-	  lineNumbers: true,
       mode: 'text/x-scala',
-      theme: $scope.theme,
-      onLoad: function(cm){
-      	$scope.themeChanged = function(){
-		  cm.setOption("theme", $scope.theme.toLowerCase());
-      	}
-      }
+      theme: 'ambiance'
     };
   });
-
