9466da504377b9cea3b8df12ce21efcc7ebced5b
Guillaume Massé <masgui@gmail.com>
2013-09-19 21:40:08 -0400

    3 pannels layout: code, insight and doc

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,25 +1,35 @@
 'use strict';
 
-angular.module('protoApp')
-  .controller('CodeCtrl', function ($scope) {
-    $scope.code = [
-    	'val a = 1 + 1',
-    	'case class Cat(age: Int, name: String){',
-    	'	def pur = s"${name * age} pur pur pur"',
-    	'}'
-    ].join('\n');
+angular.module('protoApp').controller('CodeCtrl', function ($scope) {
+  $scope.code = [
+    'case class Person(name: String, age: Int)',
+    'def isElligible(guy: Person, legalAge: Int = 18) = {',
+    '    val isAdult = guy.age > legalAge',
+    '    isAdult || guy.startsWith("Sa")',
+    '}',
+    'isElligible(Person("Sam", 2))'
+  ].join('\n');
 
+  $scope.insight = [
+    'Person(name = "foo", age = 3)',
+    'isElligible(guy = Person("foo", 3), legalAge = 18) => false',
+    'isAdult = false',
+    'false',
+    '',
+    'true'
+  ].join('\n');
+  
+  $scope.options = {
+    lineNumbers: true,
+    mode: 'text/x-scala',
+    theme: 'solarized dark',
+    lineWrapping: true
+  };
+  $scope.options2 = {
+    mode: 'text/x-scala',
+    theme: 'solarized dark',
+    lineWrapping: true,
+    readOnly: 'nocursor'
+  };
+});
 
-    $scope.insight = $scope.code;
-    $scope.options = {
-      lineNumbers: true,
-      mode: 'text/x-scala',
-      theme: 'solarized dark'
-    };
-	$scope.options2 = {
-      lineNumbers: true,
-      mode: 'text/x-scala',
-      theme: 'solarized light'
-    };
-
-  });
